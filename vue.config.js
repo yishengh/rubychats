bd4af9eb5d2dbb0eb3ed77e5fe7a4da82e628d5a
Yisheng <h.yisheng@outlook.com>
2023-11-19 16:26:29 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  publicPath: '/',
-  devServer: {
-    historyApiFallback: true,
-    allowedHosts: "all",
-    proxy: {
-      '/api': {
-        target: 'http://173.249.206.87:8888',
-        changeOrigin: true,
-        ws: false,
-        pathRewrite: {
-          '^/api': ''
+  configureWebpack: {
+    devServer: {
+      disableHostCheck: true,
+      proxy: {
+        '/api': {
+          target: 'http://173.249.206.87:8888',
+          changeOrigin: true,
+          ws: false,
+          pathRewrite: {
+            '^/api': ''
+          }
         }
       }
     }
